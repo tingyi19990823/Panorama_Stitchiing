ea08c2023ea95a1017979e9637cbbd08d6292142
tingyi <tingyi19990823@gmail.com>
2022-04-18 22:14:03 +0800

    non-maximal suppression 完成

--- feature_detector.py
+++ feature_detector.py
@@ -1,31 +1,36 @@
+from cgi import test
 from pickletools import uint8
 from cv2 import blur
 import numpy as np
 import cv2
+from numpy import unravel_index
+import time
 
 corner_threshold = 10 # 1000比較正常
+kernel_size = 10
+keypoint_count = 250
 
-# input: 要pooling的圖片
-# output: pooling完的圖片(當作遮罩使用)、特徵點數量
-def Max_Pooling(input):
-    output = np.zeros((input.shape[0],input.shape[1]))
+# input: 要pooling的圖片(灰階)、Kernel_Size
+# output: pooling完的圖片
+def Max_Pooling(input,kernel_size):
+    pooledImg = np.zeros((input.shape[0],input.shape[1]))
     max_row = 0
     max_col = 0
     corner_count = 0
-    for row in range(0,input.shape[0] - 2, 3):
-        for col in range(0,input.shape[1] - 2, 3):
+    for row in range(0,input.shape[0] - (kernel_size + 1), kernel_size):
+        for col in range(0,input.shape[1] - (kernel_size + 1), kernel_size):
             max = 0
-            for i in range(3):
-                for j in range(3):
+            for i in range(kernel_size):
+                for j in range(kernel_size):
                     if input[row + i,col + j] > max:
                         max = input[row + i,col + j]
                         max_row = row + i
                         max_col = col + j
             if max > corner_threshold:
-                output[max_row,max_col] = 255
+                pooledImg[max_row,max_col] = max
                 corner_count = corner_count + 1
-            
-    return output,corner_count
+    print('corner count after pooling: ',corner_count)
+    return pooledImg
 
 # input: 原圖、遮罩
 # output: 原圖上加紅點點的圖
@@ -40,15 +45,65 @@ def Create_CornerImg(img,maskImg):
     cornerImg[:,:,0] = np.bitwise_and(cornerImg[:,:,0],not_mask)
     return cornerImg
 
+# input: 特徵點圖片、要取的特徵點數量
+# output: 根據要的特徵點的數量輸出mask
+# 未完成
+def Non_Maximal_Suppression(input, keypoint_count):
+    start = time.clock()
+    mask = np.zeros((input.shape[0],input.shape[1]),dtype = float)
+    keypoints = 0   # 找到的特徵點數量
+    radius = 0
+    if input.shape[0] > input.shape[1]:
+        radius = input.shape[0]
+    else:
+        radius = input.shape[1]
+    while keypoints < keypoint_count:
+        radius = radius - 10
+        keypoints = 0
+        mask = np.copy(input)
+        while np.any(mask > corner_threshold) :
+            current_keypoint_index = unravel_index(mask.argmax(),input.shape)
+            x_center = current_keypoint_index[0]
+            y_center = current_keypoint_index[1]
+
+            x_offset_right = x_center + radius
+            x_offset_left = x_center - radius
+            y_offset_up = y_center - radius
+            y_offset_down = y_center + radius
+
+            if x_offset_right > input.shape[0]:
+                x_offset_right = input.shape[0]
+            if x_offset_left < 0:
+                x_offset_left = 0
+            if y_offset_up < 0:
+                y_offset_up = 0
+            if y_offset_down > input.shape[1]:
+                y_offset_down = input.shape[1]
+
+            mask[x_offset_left:(x_offset_right+1), y_offset_up:(y_offset_down+1)] = 0   # 在範圍內的都變0
+            mask[x_center,y_center] = -1                                                # 變-1，下次就不會找到
+            keypoints = keypoints + 1
+            if keypoints == keypoint_count:
+                break
+        if radius < 0:
+            break
+    mask[mask == -1] = 255
+    end = time.clock()
+    if keypoints < 500:
+        print('\n not enough keypoints , count: {} \n'.format(np.sum(mask == 255)))
+    else:
+        print('\n Non_Maximal_Suppression done,keypoint count: {}, time cost: {} \n'.format(np.sum(mask == 255),end-start))
+    return mask
 
 # input: 要detect的圖片
 # output: 灰階、遮罩、角落偵測圖片
 def Corner_detection(img):
     
     grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    blurImg = cv2.GaussianBlur(grayImg,(3,3),1.0)
 
-    Ix = cv2.Sobel(grayImg,cv2.CV_64F,1,0)
-    Iy = cv2.Sobel(grayImg,cv2.CV_64F,0,1)
+    Ix = cv2.Sobel(blurImg,cv2.CV_64F,1,0)
+    Iy = cv2.Sobel(blurImg,cv2.CV_64F,0,1)
     
     Ix2 = np.power(Ix,2)
     Iy2 = np.power(Iy,2)
@@ -64,13 +119,13 @@ def Corner_detection(img):
     HL_mat_dict[:,:,0] = Sx2
     HL_mat_dict[:,:,1] = Sy2
     HL_mat_dict[:,:,2] = Sxy
-    corner_response = (HL_mat_dict[:,:,0] * HL_mat_dict[:,:,1] - np.power(Sxy,2)) / (HL_mat_dict[:,:,0] + HL_mat_dict[:,:,1])
-
+    corner_response = (HL_mat_dict[:,:,0] * HL_mat_dict[:,:,1] - np.power(Sxy,2)) / (HL_mat_dict[:,:,0] + HL_mat_dict[:,:,1])       # det/trace
     corner_response = np.nan_to_num(corner_response)
 
-    maskImg, corner_count = Max_Pooling(corner_response)
-    print('corner count: ',corner_count)
-    cornerImg = Create_CornerImg(img,maskImg)
+    pooledImg = Max_Pooling(corner_response,kernel_size)
 
+    maskImg = Non_Maximal_Suppression(pooledImg,keypoint_count)
+
+    cornerImg = Create_CornerImg(img,maskImg)
 
     return grayImg, maskImg, cornerImg
--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+from unittest import result
 import feature_detector
 import cv2
 import os
+import time
 
-img_dir = "./pic"
+img_dir = "./grail"
 img_list = []
 
-
+result_dir = "./result_grail"
 
 if __name__ == '__main__':
+    start = time.clock()
     # 讀圖片
     for filename in os.listdir(r"./" + img_dir):
         img_list.append(cv2.imread(os.path.join(img_dir,filename)))
-
+    # 找 corner
+    index = 0
     for img in img_list:
         grayimg, maskimg, cornerimg = feature_detector.Corner_detection(img)
-        cv2.imwrite("gray.jpg",grayimg)
-        cv2.imwrite("mask.jpg",maskimg)
-        cv2.imwrite("corner.jpg",cornerimg)
+        # cv2.imwrite(os.path.join(result_dir,'gray' + str(index) + '.jpg'),grayimg)
+        cv2.imwrite(os.path.join(result_dir,'mask' + str(index) + '.jpg'),maskimg)
+        cv2.imwrite(os.path.join(result_dir,'corner' + str(index) + '.jpg'),cornerimg)
+        index = index + 1
+    end = time.clock()
+    print("total time: ",end - start)
